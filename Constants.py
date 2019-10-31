2ff6adc730530ee8f381e7d26c1e2156d73e3580
Jaime van Kessel <nallath@gmail.com>
2019-10-31 20:17:01 +0100

    Add a bit of documentation

--- Constants.py
+++ Constants.py
@@ -1,10 +1,10 @@
 
-
+# The specific heat is just a ratio. It is loosly based on real values (hence fuel being 0.5 and water being 1).
 specific_heat = {"energy": 0,
-                 "fuel": 1,
+                 "fuel": 0.5,
                  "water": 1}
 
-
+# 1 liter of this unit is how much kg?
 weight_per_unit = {"water": 1,
-                   "fuel": 1,
+                   "fuel": 0.7,
                    "energy": 0}
