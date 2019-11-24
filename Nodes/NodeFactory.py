1ead93b5ce000a856f0d3954e539e3fa9912d429
Jaime van Kessel <nallath@gmail.com>
2019-11-24 11:52:26 +0100

    Improve error logging for when serialization failed

--- Nodes/NodeFactory.py
+++ Nodes/NodeFactory.py
@@ -1,4 +1,3 @@
-import sys
 from Nodes.Node import Node
 from typing import Dict
 
@@ -9,7 +8,11 @@ class NodeFactory:
     @staticmethod
     def deserializeNode(key: str, data: Dict) -> Node:
         node_class = strToClass(data["type"])
-        return node_class(key, **data)
+        try:
+            return node_class(key, **data)
+        except TypeError as e:
+            # Re-raise it, since the original is a bit unhelpful (since it doesn't say *what* class failed)
+            raise TypeError("Unable to create %s" % node_class) from e
 
 
 
