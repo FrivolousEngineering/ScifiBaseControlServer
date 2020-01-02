c62a4209117c5ab8f08ace886d73792ddd936905
Jaime van Kessel <nallath@gmail.com>
2020-01-02 18:30:55 +0100

    Ensure that import is done before reading it from config

--- Nodes/__init__.py
+++ Nodes/__init__.py
@@ -1,12 +1,19 @@
 import sys
 from typing import Any
 
+import importlib
+
+
 def strToClass(class_name: str) -> Any:
     """
     Get a a class in the Nodes module by providing a string that identifies it
     :param class_name:
     :return:
     """
+
+    # Ensure the import was done
+    importlib.import_module(__name__ + "." + class_name)
+
     module = getattr(sys.modules[__name__], class_name)
     # Since we ensure that the module always has the same name as the class, this will work
     result_class = getattr(module, class_name)
