291849f356cda03cb29b84090cb01aea056bfa87
Jaime van Kessel <nallath@gmail.com>
2020-01-16 20:31:30 +0100

    Add oxygen as resource type

--- Nodes/Constants.py
+++ Nodes/Constants.py
@@ -2,18 +2,26 @@
 # The specific heat is just a ratio. It is loosly based on real values (hence fuel being 0.5 and water being 1).
 SPECIFIC_HEAT = {"energy": 0,
                  "fuel": 0.5,
-                 "water": 1}
+                 "water": 1,
+                 "oxygen": 0.25
+                 }
 
 # 1 liter of this unit is how much kg?
 WEIGHT_PER_UNIT = {"water": 1,
                    "fuel": 0.7,
-                   "energy": 0}
+                   "energy": 0,
+                   "oxygen": 0.001429
+                   }
 
 
 COMBUSTION_HEAT = {"water": 0,
                    "fuel": 7500,
-                   "energy": 0}
+                   "energy": 0,
+                   "oxygen": 0
+                   }
 
 BAR_COLOR = {"water": "b",
              "fuel": "r",
-             "energy": "y"}
+             "energy": "y",
+             "oxygen": "w"
+             }
