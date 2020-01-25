e9dac9d9f756060ddad7920f7bdf3bba7266e128
Jaime van Kessel <nallath@gmail.com>
2020-01-25 13:45:38 +0100

    Fix failing test

--- tests/test_FluidCooler.py
+++ tests/test_FluidCooler.py
@@ -9,7 +9,10 @@ def test_Update():
     fluid_cooler.getResourceAvailableThisTick = MagicMock(return_value = 200)
     fluid_cooler._provideResourceToOutogingConnections = MagicMock(return_value = 10)
     fluid_cooler.update()
+    # First time round it's going to provide 0 water, since it's keeping all stuff inside for one tick
+    fluid_cooler._provideResourceToOutogingConnections.assert_called_once_with("water", 0)
 
-    fluid_cooler._provideResourceToOutogingConnections.assert_called_once_with("water", 200)
-
+    fluid_cooler._provideResourceToOutogingConnections.reset_mock()
+    fluid_cooler.update()
+    fluid_cooler._provideResourceToOutogingConnections.assert_called_once_with("water", 210)
 
