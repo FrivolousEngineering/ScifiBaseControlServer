47082bef568fd4509dae11d70d785a66f1e48160
Jaime van Kessel <nallath@gmail.com>
2020-01-25 15:06:07 +0100

    Add a bunch of the nodes to the configuration
    This is so a more complex system can be tested

--- Nodes/HydroponicsBay.py
+++ Nodes/HydroponicsBay.py
@@ -24,7 +24,7 @@ class HydroponicsBay(Node):
         self._resources_left_over["energy"] = energy_available - oxygen_produced
 
         oxygen_left = self._provideResourceToOutogingConnections("oxygen", oxygen_produced)
-        print("OXYGEN", oxygen_left, oxygen_produced)
+
         self._resources_left_over["water"] += oxygen_left
         self._resources_left_over["energy"] += oxygen_left
 
