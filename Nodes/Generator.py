c50d6e9c54eb20d92062d68867e9d6825007477d
Jaime van Kessel <nallath@gmail.com>
2020-02-02 16:53:08 +0100

    Allow the generator to use other fuel types as well
    Right now we have waste & regular fuel

--- Nodes/Generator.py
+++ Nodes/Generator.py
@@ -8,9 +8,22 @@ class Generator(Node):
     Since it burns the fuel, a fair amount of heat is produced in this process. In order to cool it, it can also accept
     water, which it will use to transfer heat into.
     """
-    def __init__(self, node_id: str, **kwargs) -> None:
+    def __init__(self, node_id: str, fuel_type: str = "fuel", energy_factor: float = 1.0, **kwargs) -> None:
+        """
+        :param node_id:
+        :param fuel_type: What resource should it use as fuel?
+        :param energy_factor: How much energy should be produced with one fuel resource?
+        :param kwargs:
+        """
         super().__init__(node_id, **kwargs)
-        self._resources_required_per_tick["fuel"] = 10
+
+        # Some sanity checking.
+        if COMBUSTION_HEAT[fuel_type] == 0:
+            raise ValueError("The provided fuel type [{fuel_type} can't be burned!]".format(fuel_type = fuel_type))
+
+        self._fuel_type = fuel_type
+        self._energy_factor = energy_factor
+        self._resources_required_per_tick[fuel_type] = 10
         self._resources_required_per_tick["water"] = 250
 
         # How (in)efficient is the generator in converting the fuel it gets into heat?
@@ -25,10 +38,9 @@ class Generator(Node):
     def update(self) -> None:
         super().update()
 
-        fuel_gained = self.getResourceAvailableThisTick("fuel")
+        fuel_gained = self.getResourceAvailableThisTick(self._fuel_type)
 
-        # A generator creates 1 energy per fuel that it gets. Yay!
-        energy_available = fuel_gained * self.effectiveness_factor
+        energy_available = fuel_gained * self.effectiveness_factor * self._energy_factor
 
         # Attempt to "get rid" of the energy by offering it to connected sources.
         energy_left = self._provideResourceToOutogingConnections("energy", energy_available)
@@ -36,7 +48,7 @@ class Generator(Node):
         # So, every energy that we didn't give away also means that didn't actually result in fuel being burnt.
         # That's why we put whatever is left back into the fuel "reservoir"
         fuel_left = self.inverted_effectiveness_factor * energy_left
-        self._resources_left_over["fuel"] = fuel_left
+        self._resources_left_over[self._fuel_type] = fuel_left
 
         # We specifically use what is in the received dict (instead of the energy_available), because we want to
         # know how much was generated (and the resources available also takes leftovers into account)
@@ -44,7 +56,7 @@ class Generator(Node):
 
         # The amount of fuel we used is equal to the energy we produced. Depending on that, the generator produces heat
         inefficiency = 1.0 - self._efficiency
-        heat_produced = (fuel_gained - fuel_left) * COMBUSTION_HEAT["fuel"] * inefficiency
+        heat_produced = (fuel_gained - fuel_left) * COMBUSTION_HEAT[self._fuel_type] * inefficiency
         self.addHeat(heat_produced)
 
         # Same thing for the water. Check how much water we have.
