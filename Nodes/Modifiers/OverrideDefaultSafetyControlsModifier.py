5ec84c60f4385b69afae87117a73ce91e5cfb1d0
Jaime van Kessel <nallath@gmail.com>
2020-03-05 20:24:45 +0100

    When the OverrideDefaultSafetyControl modifier ends, ensure it's not out of bounds

--- Nodes/Modifiers/OverrideDefaultSafetyControlsModifier.py
+++ Nodes/Modifiers/OverrideDefaultSafetyControlsModifier.py
@@ -4,4 +4,8 @@ from Nodes.Modifiers.Modifier import Modifier
 class OverrideDefaultSafetyControlsModifier(Modifier):
     def __init__(self, duration):
         super().__init__(factors = {"min_performance": 0.5, "max_performance": 2}, duration = duration)
-        pass
+
+    def _onModifierRemoved(self) -> None:
+        # Set performance ensures that the limits are respected.
+        # Once this modifier gets removed, we need to ensure that the performance is in range again!
+        self._node.performance = self._node.performance
