e39bf1cde94d10371805c0e64eebc909ddf5f67c
Jaime van Kessel <nallath@gmail.com>
2020-08-03 20:35:03 +0200

    Add performance to valves
    This allows for valves to be used to control the flow of resources

--- Nodes/Valve.py
+++ Nodes/Valve.py
@@ -13,17 +13,25 @@ class Valve(ResourceStorage):
 
         # Also try to pump up resources!
         self._resources_required_per_tick[self._resource_type] = fluid_per_tick
-
+        self._min_performance = 0
         self._description = "This device pumps {resource_type} from all incomming connections and provides them to" \
                             " all of it's outgoing connections.".format(resource_type = resource_type)
 
     def _updateResourceRequiredPerTick(self) -> None:
-        new_amount_required = self._fluid_per_tick
+        new_amount_required = self._fluid_per_tick * self._performance
         storage_room_left = cast(float, self._max_storage) - self._amount
-        if storage_room_left < self._fluid_per_tick:
+        if storage_room_left < new_amount_required:
             new_amount_required = storage_room_left
-
         self._resources_required_per_tick[self._resource_type] = max(0., new_amount_required)
+
+    @ResourceStorage.performance.setter
+    def performance(self, perf) -> float:
+        self._updateResourceRequiredPerTick()
+        ResourceStorage.performance.fset(self, perf)
+
+        self._max_storage = 2 * self._performance * self._fluid_per_tick
+
+        return self._performance
 
     def update(self) -> None:
         # First, we store the resources other nodes *gave* us (these are already added!)
@@ -36,8 +44,11 @@ class Valve(ResourceStorage):
         # Now we can figure out how much we really have right now.
         self._amount = self._amount - resource_already_added + resource_available
 
+        resources_to_distribute = min(self._fluid_per_tick * self.performance, self._amount)
+        resources_left = self._amount - resources_to_distribute
+
         # Then we try to give as much away as possible.
-        self._amount = self._provideResourceToOutogingConnections(self._resource_type, self._amount)
+        self._amount = self._provideResourceToOutogingConnections(self._resource_type, resources_to_distribute) + resources_left
 
         # Finally, update how much the cooler should try to pull in.
         self._updateResourceRequiredPerTick()
--- tests/test_FluidCooler.py
+++ tests/test_FluidCooler.py
@@ -4,16 +4,12 @@ from Nodes import FluidCooler
 
 
 def test_Update():
-    fluid_cooler = FluidCooler.FluidCooler("omg", "water", 10)
+    fluid_cooler = FluidCooler.FluidCooler("omg", "water", 20)
 
     fluid_cooler.getResourceAvailableThisTick = MagicMock(return_value = 200)
     fluid_cooler._provideResourceToOutogingConnections = MagicMock(return_value = 10)
     fluid_cooler.update()
-    # It's going to provide the 200 water that it has.
-    fluid_cooler._provideResourceToOutogingConnections.assert_called_once_with("water", 200)
 
-    fluid_cooler._provideResourceToOutogingConnections.reset_mock()
-    fluid_cooler.update()
-    # And the second time it's going to provide 210.
-    fluid_cooler._provideResourceToOutogingConnections.assert_called_once_with("water", 210)
+    # It's going to provide the 20 water, since that is it's max
+    fluid_cooler._provideResourceToOutogingConnections.assert_called_once_with("water", 20)
 
