8a0b466450057ce321c431e4f52154094a36b1bd
Jaime van Kessel <nallath@gmail.com>
2020-10-18 16:42:35 +0200

    Add test for Valve

---
+++ tests/test_Valve.py
@@ -0,0 +1,19 @@
+from unittest.mock import MagicMock
+
+from Nodes import Valve
+
+
+def test_setPerformance():
+    valve = Valve.Valve("omg", "fuel", 10)
+
+    valve.getResourceAvailableThisTick = MagicMock(return_value = 10)
+
+    valve._provideResourceToOutogingConnections = MagicMock(return_value=5)
+
+    valve._performance = 0.5
+    valve._target_performance = 0.5
+
+    valve.preUpdate()
+    valve.update()
+
+    valve._provideResourceToOutogingConnections.assert_called_with("fuel", 5)
