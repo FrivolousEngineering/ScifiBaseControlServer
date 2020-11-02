98b6a880220ebf82428d797dcafc709fa796d55d
Jaime van Kessel <nallath@gmail.com>
2020-11-02 11:52:50 +0100

    Expand setPreference test for valve
    Another thing found by MutMut

--- tests/test_Valve.py
+++ tests/test_Valve.py
@@ -9,11 +9,16 @@ def test_setPerformance():
     valve.getResourceAvailableThisTick = MagicMock(return_value = 10)
 
     valve._provideResourceToOutgoingConnections = MagicMock(return_value=5)
+    assert valve.max_amount_stored == 20
 
-    valve._performance = 0.5
+    # Ensure that the _updatePerformance is actually called by having the performance *slightly* different from target
+    valve._performance = 0.5001
     valve._target_performance = 0.5
 
     valve.preUpdate()
     valve.update()
 
     valve._provideResourceToOutgoingConnections.assert_called_with("fuel", 5)
+
+    assert valve.max_amount_stored == 10
+
