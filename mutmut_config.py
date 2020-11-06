2ca6d8f9c951d440c9fe9afc5449ceaef7534a10
Jaime van Kessel <nallath@gmail.com>
2020-11-06 19:07:03 +0100

    Add some config for mutmut
    I was getting pretty fed up with it constantly trying to change the descrptions.

---
+++ mutmut_config.py
@@ -0,0 +1,10 @@
+def pre_mutation(context):
+    line = context.current_source_line.strip()
+
+    # Skip the self._description.
+    if line.startswith('self._description'):
+        context.skip = True
+
+    # Skip all the multi line strings
+    if line.startswith('"') and (line.endswith('"') or line.endswith("\\")):
+        context.skip = True
