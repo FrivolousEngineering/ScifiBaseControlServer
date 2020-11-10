1ea48d2cde388d00ceca172f64fd10271140d35f
Jaime van Kessel <nallath@gmail.com>
2020-11-10 11:57:13 +0100

    Expanded valve stubs

--- Server/ValveNamespace.py
+++ Server/ValveNamespace.py
@@ -10,9 +10,23 @@ from Server.Blueprint import api
 
 valve_namespace = Namespace("valve", description = "Valves are the remote devices that provide us with state.")
 
+valve = api.model("valve", {
+    "valve_id": fields.String
+})
+
+
 @valve_namespace.route("/")
 @valve_namespace.doc(description = "Get all the known Valves.")
 class Valves(Resource):
-    @api.response(200, "Sucess")
+    @api.response(200, "Sucess", fields.List(fields.Nested(valve)))
     def get(self):
-        return ""
+        return []
+
+
+@valve_namespace.route("/<string:valve_id>/")
+@valve_namespace.doc(params={'valve_id': 'Identifier of the valve'})
+class Node(Resource):
+    @api.response(200, "success", valve)
+    @api.response(404, "Unknown Node")
+    def get(self, node_id):
+        return {}
