32fa1a048b3ed1955ebe5ed902126102b272e2d6
Jaime van Kessel <nallath@gmail.com>
2021-01-08 18:25:46 +0100

    Tweak values of the enzyme modifier a bit

--- Nodes/Modifiers/PyrolythicRestistantEnzymeInjectorModifier.py
+++ Nodes/Modifiers/PyrolythicRestistantEnzymeInjectorModifier.py
@@ -5,7 +5,7 @@ from Nodes.Modifiers.Modifier import Modifier
 
 class PyrolythicResistantEnzymeInjectorModifier(Modifier):
     def __init__(self, duration: int) -> None:
-        super().__init__(factors = {"optimal_temperature": 0.9, "optimal_temperature_range": 2}, duration = duration)
+        super().__init__(factors = {"optimal_temperature_range": 1.5}, modifiers = {"optimal_temperature": -10}, duration = duration)
 
         self._name = "Pyrolythic Resistant Enzyme Injector"
         self._abbreviation = "PEI"
