1f0a4f77df2c756abb8c41f27f85eebe196fcd34
Jaime van Kessel <nallath@gmail.com>
2021-02-07 14:10:27 +0100

    Add toilets

---
+++ Nodes/Toilets.py
@@ -0,0 +1,30 @@
+from Nodes.Node import Node
+from Nodes.Util import enforcePositive
+
+
+class Toilets(Node):
+    """
+    Is more information really needed here? Toilets are as you imagine: They accept water and produce dirty water.
+    """
+    def __init__(self, node_id: str, **kwargs) -> None:
+        super().__init__(node_id, **kwargs)
+
+        self._use_temperature_dependant_effectiveness_factor = False
+
+        # Engineering can't really make the toilets go faster... (well, they could, but not through the engineering
+        # console). I don't want to code in "burrito night" to increase the effect of the toilets.
+        self._min_performance = 1
+        self._max_performance = 1
+
+    def update(self) -> None:
+        super().update()
+
+        water_available = self.getResourceAvailableThisTick("water")
+
+        # Technically the toilets could get damaged, but since we won't actually block players from using the toilets
+        # when they are broken in game, I'm not going to model that in here.
+        dirty_water_left = self._provideResourceToOutgoingConnections("dirty_water", water_available)
+
+        dirty_water_provided = enforcePositive(water_available - dirty_water_left)
+
+        self._resources_produced_this_tick["dirty_water"] = dirty_water_provided
