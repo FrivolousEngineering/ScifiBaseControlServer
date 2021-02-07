141ed804a3486d16d57b842108e5bc4cc022d8da
Jaime van Kessel <nallath@gmail.com>
2021-02-07 15:12:25 +0100

    Allow fluctuating resource generator to have multiple amplitudes and frequencies

--- Nodes/FluctuatingResourceGenerator.py
+++ Nodes/FluctuatingResourceGenerator.py
@@ -1,17 +1,18 @@
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 from Nodes.ResourceGenerator import ResourceGenerator
 import math
 
 
 class FluctuatingResourceGenerator(ResourceGenerator):
-    def __init__(self, node_id: str, resource_type: str, amount: float, amplitude: float, frequency: float,
+    def __init__(self, node_id: str, resource_type: str, amount: float, amplitudes: List[float], frequencies: List[float],
                  offset: float = 0, **kwargs) -> None:
         super().__init__(node_id, resource_type, amount)
         self._tick_count = 0
         self._original_amount = amount
-        self._amplitude = amplitude
-        self._frequency = frequency
+        self._amplitudes = amplitudes
+        self._frequencies = frequencies
+        assert len(self._amplitudes) == len(self._frequencies)
         self._offset = offset
 
     def serialize(self) -> Dict[str, Any]:
@@ -29,6 +30,9 @@ class FluctuatingResourceGenerator(ResourceGenerator):
         super().postUpdate()
         tick_count = (self._tick_count + self._offset) / (0.5 * math.pi)
 
-        self._amount = math.sin(10 * self._frequency * tick_count) * self._amplitude + self._original_amount
+        self._amount = self._original_amount
+
+        for frequency, amplitude in zip(self._frequencies, self._amplitudes):
+            self._amount += math.sin(10 * frequency * tick_count) * amplitude
 
         self._tick_count += 1
