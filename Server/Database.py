d12170da2a01f87a4bb2d8175f289813e6a383f0
Jaime van Kessel <nallath@gmail.com>
2021-02-26 19:01:53 +0100

    Fix missing typing

--- Server/Database.py
+++ Server/Database.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 
-def init_db():
+def init_db() -> None:
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
