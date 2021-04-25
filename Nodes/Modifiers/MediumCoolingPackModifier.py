3ba983d714551514c17f81339fee4f67dedbf26c
Jaime van Kessel <nallath@gmail.com>
2021-04-25 17:46:53 +0200

    Significantly increase values of cooling & heating packs

--- Nodes/Modifiers/MediumCoolingPackModifier.py
+++ Nodes/Modifiers/MediumCoolingPackModifier.py
@@ -4,7 +4,7 @@ from Nodes.Modifiers.HeatPerTurnModifier import HeatPerTurnModifier
 class MediumCoolingPackModifier(HeatPerTurnModifier):
     def __init__(self, duration: int) -> None:
         super().__init__(duration = duration)
-        self._heat_per_tick = -750
+        self._heat_per_tick = -2250
         self._name = "Medium Cooling Pack"
         self._abbreviation = "MCP"
         self._description = "Apply a medium chemical cooling pack to extract heat from a device."
--- Nodes/Modifiers/SmallCoolingPackModifier.py
+++ Nodes/Modifiers/SmallCoolingPackModifier.py
@@ -4,7 +4,7 @@ from Nodes.Modifiers.HeatPerTurnModifier import HeatPerTurnModifier
 class SmallCoolingPackModifier(HeatPerTurnModifier):
     def __init__(self, duration: int) -> None:
         super().__init__(duration = duration)
-        self._heat_per_tick = -250
+        self._heat_per_tick = -750
         self._name = "Small Cooling Pack"
         self._abbreviation = "SCP"
 
