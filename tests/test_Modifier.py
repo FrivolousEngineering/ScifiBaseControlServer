68a87493c5335863c32f207dbb6943e48161d087
Jaime van Kessel <nallath@gmail.com>
2021-09-18 18:28:18 +0200

    Update formating in modifier test

--- tests/test_Modifier.py
+++ tests/test_Modifier.py
@@ -6,10 +6,14 @@ from Nodes.Modifiers import Modifier
 from Nodes import Node
 
 
-@pytest.mark.parametrize("mod_1, mod_2, result", [(Modifier.Modifier(), None, False),
-                                                  (Modifier.Modifier(), Modifier.Modifier(duration = 12), False),
-                                                  (Modifier.Modifier(modifiers = {"omg": 12}), Modifier.Modifier(), False),
-                                                  (Modifier.Modifier(factors = {"omg": 12}), Modifier.Modifier(), False)])
+@pytest.mark.parametrize("""
+mod_1,                          mod_2,                              result""",[
+(Modifier.Modifier(),           None,                               False),
+(Modifier.Modifier(),           Modifier.Modifier(duration = 12),   False),
+(Modifier.Modifier(
+    modifiers = {"omg": 12}),   Modifier.Modifier(),                False),
+(Modifier.Modifier(
+    factors = {"omg": 12}),     Modifier.Modifier(),                False)])
 def test_compare(mod_1, mod_2, result):
     assert (mod_1 == mod_2) == result
 
