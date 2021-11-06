6ab6539233f91e54f6ae1c42067da0334e91ff9f
Jaime van Kessel <nallath@gmail.com>
2021-11-06 15:42:51 +0100

    Expand tests for OilExtractor

--- tests/test_OilExtractor.py
+++ tests/test_OilExtractor.py
@@ -1,5 +1,7 @@
 import math
+from unittest.mock import patch
 
+from Nodes import OilExtractor
 from tests.testHelpers import createEngineFromConfig
 import pytest
 
@@ -19,9 +21,9 @@ def setupForIdealState(config_file):
 
 @pytest.mark.parametrize("performance", [1, 1.5, 0.8])
 @pytest.mark.parametrize("sub_ticks", [1, 10, 30])
-@pytest.mark.parametrize('ticks', [1])
+@pytest.mark.parametrize('ticks', [1, 10, 20])
 @pytest.mark.parametrize("config_file, oil_created_per_tick", [("OilExtractor.json", 5)])
-def test_medicineProduced(config_file, oil_created_per_tick, sub_ticks, ticks, performance):
+def test_medicineProduced_noHeat(config_file, oil_created_per_tick, sub_ticks, ticks, performance):
     engine = setupForIdealState(config_file)
     engine._sub_ticks = sub_ticks
     oil_extractor = engine.getNodeById("oil_extractor")
@@ -30,8 +32,9 @@ def test_medicineProduced(config_file, oil_created_per_tick, sub_ticks, ticks, p
 
     oil_extractor._setPerformance(performance)
     oil_extractor.target_performance = performance
-    for _ in range(ticks):
-        engine.doTick()
+    with patch.dict(OilExtractor.COMBUSTION_HEAT, {"fuel": 0}):  # Don't add heat from burned up fuel
+        for _ in range(ticks):
+            engine.doTick()
 
     assert math.isclose(engine.getNodeById("plant_oil_storage").amount_stored,
                         oil_created_per_tick * ticks * performance)
