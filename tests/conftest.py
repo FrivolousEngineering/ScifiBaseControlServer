56782d5615e42678bbac10cb2526119f0a7335c1
Jaime van Kessel <nallath@gmail.com>
2022-01-30 11:56:35 +0100

    Fix spelling mistake in comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import signal
 
 
 # Yeah, this is hacky af. Since the pytest_terminal_summary is pretty much the last thing that gets done, we want to
-# make absolutely sure that the servers we started for the serverAPI test get killed. That should have already happend
+# make absolutely sure that the servers we started for the serverAPI test get killed. That should have already happened
 # but if we skipped the test (because we only wanted to run a few tests) the subprocesses would never be killed.
 @pytest.hookimpl()
 def pytest_terminal_summary():
