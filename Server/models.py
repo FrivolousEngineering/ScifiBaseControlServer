6bcfb4d1f510dfc9ed5582b261c6c0769af092d8
Jaime van Kessel <nallath@gmail.com>
2022-07-02 11:29:42 +0200

    Add modifier to database
    We need this in order to be able to track what user has what modifiers active

--- Server/models.py
+++ Server/models.py
@@ -8,6 +8,11 @@ user_ability_table = Table('user_ability', Base.metadata,
     Column('ability_id', Integer, ForeignKey('ability.id'))
 )
 
+user_modifier_table = Table('user_modifier', Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.card_id')),
+    Column('modifier_id', Integer, ForeignKey('modifier.id'))
+)
+
 
 class User(Base):  # type: ignore
     __tablename__ = 'user'
@@ -17,6 +22,7 @@ class User(Base):  # type: ignore
     email = Column(String(120), unique=True)
 
     abilities = relationship("Ability", secondary = user_ability_table, backref = "users")
+    active_modifiers = relationship("Modifier", secondary = user_modifier_table)
 
     def __init__(self, card_id=None, name=None, email=None):
         self.card_id = card_id
@@ -25,6 +31,20 @@ class User(Base):  # type: ignore
 
     def __repr__(self):
         return '<User %r>' % self.name
+
+
+class Modifier(Base):
+    __tablename__ = 'modifier'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))  # What is the name of the modifier that was placed?
+    node_id = Column(String(100))  # On what node is this placed?
+    card_id = Column(String(50))  # By which card was this modifier placed?
+
+    def __init__(self, name=None, node_id=None, card_id = None):
+        self.name = name
+        self.node_id = node_id
+        self.card_id = card_id
 
 
 class Ability(Base):  # type: ignore
