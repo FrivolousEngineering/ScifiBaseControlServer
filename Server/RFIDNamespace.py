c72eba060b2916e5c7d916cf3523bdefcfa37519
Jaime van Kessel <nallath@gmail.com>
2022-07-03 14:18:26 +0200

    Update API endpoints for RFID cards

--- Server/RFIDNamespace.py
+++ Server/RFIDNamespace.py
@@ -3,7 +3,7 @@ from flask_restx import Resource, Api, apidoc, fields, Namespace, Model
 from flask import current_app as app
 from Server.Blueprint import api
 from Server.models import User, Ability, AccessCard
-from Server.Database import db_session
+from Server.Database import db_session, getDBSession
 from sqlalchemy import exc
 
 from flask import Response, request
@@ -12,14 +12,26 @@ RFID_namespace = Namespace("RFID", description = "Users can authenticate themsel
 
 
 UNKNOWN_CARD_RESPONSE = Response('{"message": "Unknown Card"}', status=404, mimetype='application/json')
-CARD_UPDATE_FAILED = Response('{"message": "User update failed"}', status=500, mimetype='application/json')
-CARD_UPDATE_SUCCEEDED = Response('{"message": "User updated"}', status=200, mimetype='application/json')
+CARD_UPDATE_FAILED = Response('{"message": "Card update failed"}', status=500, mimetype='application/json')
+CARD_UPDATE_SUCCEEDED = Response('{"message": "Card updated"}', status=200, mimetype='application/json')
+CARD_ADDED = Response('{"message": "Card Added"}', status=201, mimetype='application/json')
+
+UNKNOWN_USER = Response('{"message": "The provided username was not found. Please create the user first!"}',
+                        status = 404,
+                        mimetype='application/json')
+
+user_parser = api.parser()
+user_parser.add_argument('user_name',
+                         type = str,
+                         help = 'Username that needs to be associated with the RFID badge',
+                         location = 'form',
+                         required = True)
 
 
 @RFID_namespace.route("/<string:card_id>/")
 @RFID_namespace.doc(description ="Get RFID card data")
 class RFID(Resource):
-    @api.response(200, "success")
+    @api.response(200, "Success")
     @api.response(404, "Unknown Card")
     def get(self, card_id):
         access_card = AccessCard.query.filter_by(id = card_id).first()
@@ -28,11 +40,45 @@ class RFID(Resource):
         else:
             return {"user_name": access_card.user.id}
 
+    @api.response(201, "Card was added to the database")
+    @api.response(409, "Card already exists, unable to add it again")
+    @api.response(404, "User not found")
+    @api.expect(user_parser)
+    def post(self, card_id):
+        access_card = AccessCard.query.filter_by(id=card_id).first()
+        if access_card:
+            return Response('{"message": "The provided card already exists. Unable to add it again"}',
+                            status=409,
+                            mimetype='application/json')
 
+        user = User.query.filter_by(id = request.form["user_name"]).first()
+        if not user:
+            return UNKNOWN_USER
+
+        user.access_cards.append(AccessCard(card_id))
+
+        getDBSession().commit()
+        return CARD_ADDED
+
+    @api.response(404, "Unknown Card")
+    @api.response(404, "Unknown User")
+    @api.response(200, "Card Updated")
+    @api.expect(user_parser)
+    def put(self, card_id):
+        access_card = AccessCard.query.filter_by(id=card_id).first()
+        if not access_card:
+            return UNKNOWN_CARD_RESPONSE
+        user = User.query.filter_by(id=request.form["user_name"]).first()
+        if not user:
+            return UNKNOWN_USER
+
+        access_card.user = user
+        getDBSession().commit()
+        return CARD_UPDATE_SUCCEEDED
 
 # name, email are required for new users. Ability can be passed multiple times.
 # Only adds abilities; does not remove them.
-@RFID_namespace.route("/update/<string:card_id>/")
+'''@RFID_namespace.route("/update/<string:card_id>/")
 @RFID_namespace.doc(description = "Update a user's information")
 class RFIDUpdate(Resource):
     @api.response(200, "User updated")
@@ -58,5 +104,5 @@ class RFIDUpdate(Resource):
         else:
             user.abilities += abilities
             db_session.commit()
-            return CARD_UPDATE_SUCCEEDED
+            return CARD_UPDATE_SUCCEEDED'''
 
