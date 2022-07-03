90778cf2811afef1203c7fe2ee124fa6f7af0633
Jaime van Kessel <nallath@gmail.com>
2022-07-03 16:34:52 +0200

    Fix mypy checks

--- Nodes/Lights.py
+++ Nodes/Lights.py
@@ -5,15 +5,15 @@ class Lights(ResourceDestroyer):
     """
     Lights simulate
     """
-    def __init__(self, node_id: str, **kwargs) -> None:
+    def __init__(self, node_id: str, amount: float, **kwargs) -> None:
         """
         :param node_id: Unique identifier of the node
         :param amount: How much power do the lights need per tick?
         :param kwargs:
         """
-        defaults = {"resource_type": "energy"}
+        defaults = {}
         defaults.update(kwargs)
-        super().__init__(node_id, **defaults)
+        super().__init__(node_id, resource_type = "energy", amount = amount, **defaults)
 
         self._description = "A set of lights that ensure that an area can be worked in. If not enough energy is " \
                             "provided, the lights will jump to 'backup' power mode. It's still possible to work when" \
