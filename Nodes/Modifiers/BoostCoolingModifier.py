c483ce0a04d25d5baa0a76370d86de3fb5fdc09a
Jaime van Kessel <nallath@gmail.com>
2022-08-02 15:12:00 +0200

    Update description of boost cooling modifier a bit

--- Nodes/Modifiers/BoostCoolingModifier.py
+++ Nodes/Modifiers/BoostCoolingModifier.py
@@ -6,5 +6,6 @@ class BoostCoolingModifier(Modifier):
         super().__init__(factors = {"heat_emissivity": 1.5, "heat_convection_coefficient": 4}, duration = duration)
         self._name = "Boost cooling"
         self._abbreviation = "BOC"
-        self._description = "Significantly increase the amount of heat that is emitted and greatly increase the amount" \
-                            " of heat that is lost due to convection."
+        self._description = "Significantly increase the amount of heat that is emitted and greatly increase the " \
+                            "amount of heat that is lost due to convection. This works best on devices that have a " \
+                            "large surface area and / or high heat convection "
